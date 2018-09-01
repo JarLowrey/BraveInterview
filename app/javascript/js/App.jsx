8caeec022c97c1ea69a6942f597579901de5600c
James <jtronlabs@gmail.com>
2018-09-01 13:39:48 -0700

    react-router is serving an Index route, App.jsx matches paths to routes

--- app/javascript/js/App.jsx
+++ app/javascript/js/App.jsx
@@ -1,12 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-export default class App extends Component{
-    render(){
-        return (
-            <div>
-               <p> asdf</p>
-            </div>
-        );
-    }
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import Index from "./routes/index";
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Index} />
+        </Switch>
+      </Router>
+    );
+  }
 }
