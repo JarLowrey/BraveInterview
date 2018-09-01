1ccdb669118b1859541d8a90bb7b06fb7f3da913
James <jtronlabs@gmail.com>
2018-09-01 13:50:54 -0700

    added mui

--- app/javascript/js/App.jsx
+++ app/javascript/js/App.jsx
@@ -2,16 +2,20 @@ import React, { Component } from "react";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
 import Index from "./routes/index";
 
 export default class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Index} />
-        </Switch>
-      </Router>
+      <MuiThemeProvider theme={createMuiTheme()}>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Index} />
+          </Switch>
+        </Router>
+      </MuiThemeProvider>
     );
   }
 }
