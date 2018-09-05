4e6606b788e19897668b5276644fa332249eaf13
James <jtronlabs@gmail.com>
2018-09-05 14:19:23 -0700

    beginning of a search form

--- app/javascript/js/routes/index.jsx
+++ app/javascript/js/routes/index.jsx
@@ -1,9 +1,64 @@
 import React, { Component } from "react";
 
+import TextField from '@material-ui/core/TextField';
+
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
 export default class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+    this.handleTypeChange = this.handleTypeChange.bind(this);
+
+    this.state = {
+      search: "",
+      type: 10
+    }
+  }
+
+  handleSearchChange(event) {
+    this.setState({ search: event.target.value });
+  }
+  handleTypeChange(event) {
+    this.setState({ type: event.target.value });
+  }
+
   render() {
     return (
-        <div>asdfasdasd</div>
+        <form noValidate autoComplete="off">
+        <span>Search </span>
+          <FormControl
+          style={{verticalAlign:'bottom'}}
+            margin="normal">
+            <InputLabel htmlFor="resource-search-type--id">Resource</InputLabel>
+            <Select
+              value={this.state.type}
+              style={{ minWidth: '150px' }}
+              onChange={this.handleTypeChange}
+              input={<Input name="resource-search-type" id="resource-search-type--id" />}
+            >
+              <MenuItem value={10}>People</MenuItem>
+              <MenuItem value={20}>Films</MenuItem>
+              <MenuItem value={30}>Starships</MenuItem>
+              <MenuItem value={40}>Species</MenuItem>
+              <MenuItem value={50}>Planets</MenuItem>
+            </Select>
+          </FormControl>
+
+          <span> for </span>
+
+          <TextField
+            id="swapi-search"
+            label="Search Term"
+            value={this.state.search}
+            onChange={this.handleSearchChange}
+            margin="normal"
+          />
+        </form>
     );
   }
 }
